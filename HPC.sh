0180c22b15378b94cf1563b98dbb46e67a222349
jtrevorrow b35t <jtrevorrow.b35t@login01.head.cm.i3.isambard.ac.uk>
2026-09-24 09:08:30 +0000

    Modified HPC.sh

--- HPC.sh
+++ HPC.sh
@@ -3,13 +3,11 @@
 #SBATCH --job-name=ia23938-hcva
 #SBATCH --output=hcva.out
 #SBATCH --error=hcva.err
-#SBATCH --time=00:10:00
-#SBATCH --mem=32G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=300G
+#SBATCH --nodes=6
+#SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=72
-
-# An hour into computation mem use is sitting at around 8 gig, so can request much less than 100G
 
 cd "${SLURM_SUBMIT_DIR}"
 
@@ -33,13 +31,14 @@ mkdir -p "$JULIA_DEPOT_PATH"
 
 # These lines should do same thing:
 #julia -e 'using Pkg; Pkg.activate("/lfs1i3/home/b35t/jtrevorrow.b35t/miniforge3/envs/hcva/julia_env/Project.toml"); include("lp_regression/IRLS-pNorm.jl")'
-python -c "from runner import julia_init; julia_init()"
 
 echo setting PYTHON_JULIAPKG_EXE
 
 export PYTHON_JULIAPKG_EXE=$HOME/julia-1.11.7/bin/julia
-
+echo "PYTHON_JULIAPKG_EXE=$PYTHON_JULIAPKG_EXE"
+echo "PATH=$PATH"
 echo Starting Python
+python -c "from runner import julia_init; julia_init()" || { echo "Julia pre-warm failed"; exit 1; }
 
 # Experiment ``vary_grp_fact'''
 python -O runner.py -values_dir "value_systems/Synthetic/vary_grp_fact" "value_systems/Synthetic/vary_mup_vamu" "value_systems/Synthetic/vary_prip_grp_fact" "value_systems/Synthetic/vary_sigma_prip" "value_systems/Synthetic/vary_sigma_prip_MINIMISE" "value_systems/Synthetic/randoms" -n_values 4 -n_actions 2 -n_workers 42
